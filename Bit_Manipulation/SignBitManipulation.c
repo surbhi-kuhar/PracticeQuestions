c7633bffd24050a4fae7b0d3a0e6b4abcdbf300b
surbhi-kuhar <surbhikuhar07@gmail.com>
2023-12-24 13:29:26 +0530

    Sign Bit using bits in C

--- Bit_Manipulation/SignBitManipulation.c
+++ Bit_Manipulation/SignBitManipulation.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int sign(int i)
-{
-	return 1 + (i >> 31) - (-i >> 31);
+int sign(int x) 
+{ 
+    return (x >> 31) | (!(!x)); 
 }
-
 
 int main() {
     
@@ -12,15 +11,7 @@ int main() {
     scanf("%d",&num);
     int result = sign(num);
     
-    if(result==0){
-        printf("sign(%d) = -1 \n",num);
-    }
-    else if(result==1){
-        printf("sign(%d) = 0 \n",num);
-    }
-    else if(result==2){
-        printf("sign(%d) = 1 \n",num);
-    }
+    printf("sign(%d) = %d \n",num, result);
 
     return 0;
 }
